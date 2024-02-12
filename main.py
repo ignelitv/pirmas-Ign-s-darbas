3530b2500dc3fb9ca48f2aa24741f027fff02323
ignelitv <159789542+ignelitv@users.noreply.github.com>
2024-02-12 16:45:46 +0200

    Update main.py
    skaičiuotuvas

--- main.py
+++ main.py
@@ -14,3 +14,19 @@ def dalyba(x,y)
 print("Pasirinkite operacija:")
 print("1. Sudetis")
 print("2. Atimtis")
+print("3. Daugyba")
+print("4. Dalyba")
+
+while true:
+    pasirinkimas = input("Iveskite pasirinkima (1/2/3/4): ")
+
+    if pasirinkimas in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("iveskite pirmaji skaiciu: "))
+            num2 = float(input("iveskite antraji skaiciu: "))
+        except valueerror:
+            print("Neteisinga ivestis. Pasirinkite dar karta.")
+            continue
+
+        if pasirinkimas == '1':
+            print(num1, "+", num2, "=" sudetis(num1, num2))

76cf7471262a4946aac7e49a2814aa9a1bd3f48a
ignelitv <159789542+ignelitv@users.noreply.github.com>
2024-02-12 16:33:01 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+
+def sudetis(x,y):
+    return x + y
+
+def atimtis(x,y):
+    return x - y
+
+def daugyba(x,y)
+    return x * y
+
+def dalyba(x,y)
+    return x / y
+
+print("Pasirinkite operacija:")
+print("1. Sudetis")
+print("2. Atimtis")
